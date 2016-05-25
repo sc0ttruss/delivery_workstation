3120cfe75a51d10d139fe48a89b95b1a2ff8cfc3
scott <scott@vertex.(none)>
2016-05-25 16:47:18 +0300

    add the knife.rb with supermarket url

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -8,7 +8,7 @@ client_key               "~/chef-kits/chef/delivery.pem"
 validation_client_name   "myorg-validator"
 validation_key           "~/chef-kits/chef/myorg-validator.pem"
 chef_server_url          "https://chef.myorg.chefdemo.net/organizations/myorg"
+knife[:supermarket_site] = 'https://supermarket.myorg.chefdemo.net'
 cache_type               'BasicFile'
 cache_options( :path => "#{ENV['HOME']}/.chef/checksums" )
 cookbook_path            ["#{current_dir}/../cookbooks"]
-
